526362307b0b0353671a0ff1b2b941f6ba90a478
mifta04 <mariasiaw753@gmail.com>
2019-03-21 18:55:24 +0700

    Tugas-ViewPager

---
+++ app/src/main/java/com/mifta/android3/MyPagerAdapter.kt
@@ -0,0 +1,31 @@
+package com.mifta.android3
+
+import android.support.v4.app.Fragment
+import android.support.v4.app.FragmentManager
+import android.support.v4.app.FragmentPagerAdapter
+
+
+
+class MyPagerAdapter(fm: FragmentManager): FragmentPagerAdapter(fm){
+    // sebuah list yang menampung objek Fragment
+    private val pages = listOf(
+            FirstFragment(), SecondFragment(), ThridFragment(), fourthFragment ()
+    )
+    // menentukan fragment yang akan dibuka pada posisi tertentu
+    override fun getItem(position: Int): Fragment {
+        return pages[position]
+    }
+    override fun getCount(): Int {
+        return pages.size
+    }
+    // judul untuk tabs
+    override fun getPageTitle(position: Int): CharSequence? {
+        return when(position){
+            0 -> "Landmark Dunia"
+            1 -> "Landmark Indonesia"
+            2 -> "Apa itu Landmark?"
+            else -> "profil"
+        }
+
+    }
+}
